26bce0439ff9c9ec1f3d3dd2f99fd97ecfd33914
Touseef Ahmad <touseef.ahmad.cs@gmail.com>
2020-02-02 09:17:01 +0500

    merge linked list partially solved

--- 7.1: merge_sorted_link_list.py
+++ 7.1: merge_sorted_link_list.py
@@ -21,4 +21,36 @@ def print_list(node):
 list = LinkedList(7)
 list.insert(5)
 list.insert(2)
-print_list(list.head)
+list2 = LinkedList(11)
+list2.insert(3)
+
+def merge(list1 , list2):
+
+    p1 = list1.head
+    p2 = list2.head
+    if p1.data < p2.data:
+        list3 = LinkedList(p1.data)
+        p1 = p1.next
+    else:
+        list3 = LinkedList(p2.data)
+        p2 = p2.next
+
+    while p1 or p2:
+        if not p1:
+            list3.insert(p2.data)
+            p2 = p2.next
+            continue
+        if not p2:
+            list3.insert(p1.data)
+            p1 = p1.next
+            continue
+        if p1.data < p2.data:
+            list3.insert(p1.data)
+            p1 = p1.next
+        else:
+            list3.insert(p2.data)
+            p2 = p2.next
+    return list3
+
+list3 = merge(list,list2)
+print_list(list3.head)
