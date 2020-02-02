1dd29e350a9219ea99de5ad90fec835515dc6a43
Touseef Ahmad <touseef.ahmad.cs@gmail.com>
2020-02-02 08:50:39 +0500

    implemented linked list with insert and print

--- 7.1: merge_sorted_link_list.py
+++ 7.1: merge_sorted_link_list.py
@@ -1,1 +1,24 @@
-# TODO
+class Node:
+    def __init__(self , data=0 , next=None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self , data):
+        self.head = Node(data)
+
+    def insert(self , data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+def print_list(node):
+    print(node.data)
+    if not node.next:
+        return
+    print_list(node.next)
+
+list = LinkedList(7)
+list.insert(5)
+list.insert(2)
+print_list(list.head)
