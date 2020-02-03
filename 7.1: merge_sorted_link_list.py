7e06d817c04e000f50b3ba7ddcc4f606fb345321
Touseef Ahmad <touseef.ahmad.cs@gmail.com>
2020-02-03 21:26:11 +0500

    merge sorted list

--- 7.1: merge_sorted_link_list.py
+++ 7.1: merge_sorted_link_list.py
@@ -1,56 +1,34 @@
-class Node:
+class LinkedList:
     def __init__(self , data=0 , next=None):
         self.data = data
         self.next = next
+    def insert_after(self, new_node):
+        new_node.next = self.next
+        self.next = new_node
 
-class LinkedList:
-    def __init__(self , data):
-        self.head = Node(data)
+def print_list(list):
+    while list:
+        print(list.data)
+        list = list.next
 
-    def insert(self , data):
-        new_node = Node(data)
-        new_node.next = self.head
-        self.head = new_node
+list1 = LinkedList(2)
+list1.insert_after(LinkedList(5))
+list1.insert_after(LinkedList(7))
+list2 = LinkedList(3)
+list2.insert_after(LinkedList(11))
 
-def print_list(node):
-    print(node.data)
-    if not node.next:
-        return
-    print_list(node.next)
+def merge(L1 , L2):
 
-list = LinkedList(7)
-list.insert(5)
-list.insert(2)
-list2 = LinkedList(11)
-list2.insert(3)
+    dummy_head = tail = LinkedList()
 
-def merge(list1 , list2):
-
-    p1 = list1.head
-    p2 = list2.head
-    if p1.data < p2.data:
-        list3 = LinkedList(p1.data)
-        p1 = p1.next
-    else:
-        list3 = LinkedList(p2.data)
-        p2 = p2.next
-
-    while p1 or p2:
-        if not p1:
-            list3.insert(p2.data)
-            p2 = p2.next
-            continue
-        if not p2:
-            list3.insert(p1.data)
-            p1 = p1.next
-            continue
-        if p1.data < p2.data:
-            list3.insert(p1.data)
-            p1 = p1.next
+    while L1 and L2:
+        if L1.data < L2.data:
+            tail.next , L1 = L1 , L1.next
         else:
-            list3.insert(p2.data)
-            p2 = p2.next
-    return list3
+            tail.next , L2 = L2 , L2.next
+        tail = tail.next
 
-list3 = merge(list,list2)
-print_list(list3.head)
+    tail.next = L1 or L2
+    return dummy_head.next
+
+print_list(merge(list1,list2))
